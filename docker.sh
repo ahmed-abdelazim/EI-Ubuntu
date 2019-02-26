ff4301856b1977612e16c71f886e68ab2e59123e
ahmed-abdelazim <34234821+ahmed-abdelazim@users.noreply.github.com>
2019-02-26 11:36:05 +0200

    Update docker.sh
    removed -y from key add

--- docker.sh
+++ docker.sh
@@ -7,7 +7,7 @@ sudo apt-get -y install \
     gnupg-agent \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key -y fingerprint 0EBFCD88
+sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
